bcbabc32ae3d3f04d70223ea7aafedc3fb75dd3c
PinedaVictor <pinedavictor095@gmail.com>
2024-03-28 00:04:36 -0700

    Added pr and smz prompt

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -63,5 +63,5 @@ func init() {
 	// will be global for your application.
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
