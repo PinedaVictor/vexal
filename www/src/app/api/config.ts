f1e553dbac30c3a1f073704c25356e9f660d0de7
PinedaVictor <pinedavictor095@gmail.com>
2024-05-22 23:45:11 -0700

    Added secrets manager to o nextjs server - looks like it's not working with edge rungtime

---
+++ www/src/app/api/config.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  // OAuth2.0
+  CALLBACK_URL: z.string(),
+
+  // Firebase
+  NEXT_PUBLIC_FB_apiKey: z.string(),
+  NEXT_PUBLIC_FB_authDomain: z.string(),
+  NEXT_PUBLIC_FB_projectId: z.string(),
+  NEXT_PUBLIC_FB_storageBucket: z.string(),
+  NEXT_PUBLIC_FB_messagingSenderId: z.string(),
+  NEXT_PUBLIC_FB_appId: z.string(),
+  NEXT_PUBLIC_FB_measurementId: z.string(),
+
+  // Firebase Admin
+  FB_ADMIN_PROJECT_ID: z.string(),
+  FB_ADMIN_CLIENT_EMAIL: z.string(),
+  FB_ADMIN_PRIVATE_KEY: z.string(),
+
+  // GCP
+  GCP_PROJECT_ID: z.string(),
+
+  // Secreats Manager
+  SECRETS_MAN_PRIVATE_KEY: z.string(),
+  SECRETS_MAN_EMAIL: z.string(),
+});
+
+export const API_ENV = envSchema.parse(process.env);
