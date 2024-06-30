255e77b27ce36102e746faa118e413c7b75890c8
PinedaVictor <pinedavictor095@gmail.com>
2024-06-29 21:01:06 -0700

    Checking if PR template exists

--- internal/pr/pr.go
+++ internal/pr/pr.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"vx/pkg"
 	"vx/pkg/exe"
+	"vx/pkg/paths"
 
 	"github.com/google/go-github/v62/github"
 )
@@ -21,11 +22,10 @@ func AutoPr(branch string) {
 	// draft := false
 	// issue := 0
 	pullReq := &github.NewPullRequest{
-		Title:    &workingBranch,
-		Head:     github.String(workingBranch),
-		HeadRepo: github.String(workingBranch),
-		Base:     github.String(branch),
-		// Base:                &base,
+		Title:               &workingBranch,
+		Head:                github.String(workingBranch),
+		HeadRepo:            github.String(workingBranch),
+		Base:                github.String(branch),
 		Body:                &prBody,
 		MaintainerCanModify: &maintainerCanModify,
 		// Draft:               &draft,
@@ -39,4 +39,11 @@ func AutoPr(branch string) {
 	}
 	url := pullRequest.HTMLURL
 	exe.OpenURL(*url)
+}
+
+func HasPRTemplate() {
+	dir, _ := os.Getwd()
+	fmt.Println("Working directory:", dir)
+	tplExists := paths.PathExists(fmt.Sprintf("%s/.github/pull_request_template.md", dir))
+	fmt.Println("TPL Exists:", tplExists)
 }
