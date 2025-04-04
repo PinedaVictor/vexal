404a1b9fb4940dfe440451e54a6c5a4a8e89e632
PinedaVictor <pinedavictor095@gmail.com>
2025-04-04 03:22:43 -0700

    Added surge ejs command, and updated default done message in loader

--- cmd/surge.go
+++ cmd/surge.go
@@ -4,6 +4,10 @@ Copyright © 2025 Victor Pineda pinedavictor095@gmail.com
 package cmd
 
 import (
+	"fmt"
+	"vx/internal"
+
+	pluto "github.com/PinedaVictor/pluto/surge/ejs"
 	"github.com/spf13/cobra"
 )
 
@@ -14,8 +18,31 @@ var surgeCmd = &cobra.Command{
 	Long:  `Server generation for different libraries and frameworks.`,
 }
 
+var name = ""
+var exjs = &cobra.Command{
+	Use:   "ejs",
+	Short: "Generate an ExpressJS server",
+	Long: `
+	This command generates an ExpressJS server with the dependencies express, ts-node,and nodemon,
+	along with configurations for Docker, Prettier, ESLint, and both development and build scripts.
+	`,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		var msg = fmt.Sprintf("Spawning server: %s", name)
+		internal.StartSpinner(msg)
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		pluto.SpawnServer(name)
+
+	},
+	PostRun: func(cmd *cobra.Command, args []string) {
+		internal.StopSpinner(internal.DftlDoneMsg)
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(surgeCmd)
+	surgeCmd.AddCommand((exjs))
+	exjs.Flags().StringVarP(&name, "name", "n", "server", "Name of server you want to generate")
 
 	// Here you will define your flags and configuration settings.
 
